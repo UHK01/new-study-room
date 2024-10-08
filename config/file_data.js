807464ef3a36883875016adf22360239fae25fed
UHK01 <tayuuki222@gmail.com>
2024-10-09 01:12:24 +0900

    pre1

---
+++ config/file_data.js
@@ -0,0 +1,29 @@
+const file_data = [
+    {
+        id:1,
+        file_name:"歴史総合プリント答え",
+        file_path: [
+            "../data/rekisi/1.jpeg",
+            "../data/rekisi/1.jpeg",
+            "../data/rekisi/1.jpeg",
+            "../data/rekisi/1.jpeg",
+            "../data/rekisi/1.jpeg",
+        ],
+        file_pdf_path:"../data/rekisi/2.pdf",
+        status:0,
+        tags:"歴史総合",
+    }
+    ,
+    {
+        id:2,
+        file_name:"歴史総合プリント答え",
+        file_path: [
+            "../data/rekisi/1.jpeg",
+        ],
+        file_pdf_path:"../data/rekisi/2.pdf",
+        status:2,
+        tags:"歴史総合",
+    }
+]
+
+//status = 0=open 1=private 2=delete
