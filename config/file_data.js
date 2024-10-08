432841fb477ef14a8059aad38850de1007519a72
UHK01 <tayuuki222@gmail.com>
2024-10-09 02:01:19 +0900

    pre5

--- config/file_data.js
+++ config/file_data.js
@@ -1,23 +1,12 @@
 const file_data = [
     {
         id:1,
-        file_name:"歴史総合プリント答え",
+        file_name:"歴史総合プリント",
         file_path: [
-            "../data/rekisi/1.jpeg",
+            "../data/rekisi/1.jpg",
         ],
-        file_pdf_path:"../data/rekisi/2.pdf",
+        file_pdf_path:"../data/rekisi/1.pdf",
         status:0,
-        tags:"歴史総合",
-    }
-    ,
-    {
-        id:2,
-        file_name:"歴史総合プリント答え",
-        file_path: [
-            "../data/rekisi/1.jpeg",
-        ],
-        file_pdf_path:"../data/rekisi/2.pdf",
-        status:2,
         tags:"歴史総合",
     }
 ]
