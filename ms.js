861d3cabbde78ff956022608998f84295429ccc3
UHK01 <tayuuki222@gmail.com>
2024-12-12 20:20:58 +0900

    pre90

---
+++ ms.js
@@ -0,0 +1,3 @@
+let ms = false;
+// let ms_message = '<div class="main"><h2 class="error">緊急メンテナンス中</h2><div class="error">申し訳ございません。ただいまメンテナンス中です。<br>メンテナンス終了まで今しばらくお待ち下さい。</div><br><div class="link"><a href="/" class="link">ホームに戻る</a></div></div>';
+let ms_message = '<div class="main"><h2 class="error">緊急メンテナンス中</h2><br><div class="link"><a href="/" class="link">ホームに戻る</a></div></div>';
